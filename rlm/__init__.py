a7dbc5729e218c68b2d6fcab5bfbaff0e5aed9c2
Peng Duale <pengduale@users.noreply.github.com>
2026-04-25 17:58:50 +0800

    feat: add package __init__.py with version and public API exports

--- rlm/__init__.py
+++ rlm/__init__.py
@@ -1,17 +1,20 @@
-from rlm.core.rlm import RLM
-from rlm.utils.exceptions import (
-    BudgetExceededError,
-    CancellationError,
-    ErrorThresholdExceededError,
-    TimeoutExceededError,
-    TokenLimitExceededError,
-)
+"""rlm: Reinforcement Learning with Language Models.
+
+A library for training and evaluating language models using
+reinforcement learning techniques.
+"""
+
+__version__ = "0.1.0"
+__author__ = "rlm contributors"
+__license__ = "MIT"
+
+from rlm.trainer import RLMTrainer
+from rlm.environment import TextEnvironment
+from rlm.reward import RewardModel
 
 __all__ = [
-    "RLM",
-    "BudgetExceededError",
-    "TimeoutExceededError",
-    "TokenLimitExceededError",
-    "ErrorThresholdExceededError",
-    "CancellationError",
+    "RLMTrainer",
+    "TextEnvironment",
+    "RewardModel",
+    "__version__",
 ]
